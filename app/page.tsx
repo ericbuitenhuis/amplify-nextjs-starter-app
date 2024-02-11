c54b2b5ce8dd9e5eab8f3b3d1e7b9950250fe910
Eric Buitenhuis <Eric.Buitenhuis@va.gov>
2024-02-11 14:27:47 -0500

    made changes

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.description}>
         <p>
-          Get started by editing&nbsp;
+          Get started by saying hello world and by editing&nbsp;
           <code className={styles.code}>app/page.tsx</code>
         </p>
       </div>
